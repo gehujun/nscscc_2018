3fb2e3202aa77f422d43e0d09677a5724c36227f
gehujun <1319579758@qq.com>
2018-07-28 15:06:55 +0800

    添加移位指令

--- rtl/maindec.v
+++ rtl/maindec.v
@@ -33,12 +33,12 @@ module maindec(
 	assign {regwrite,regdst,alusrc,branch,memwrite,memtoreg,jump,aluop} = controls;
 	always @(*) begin
 		case (op)
-			6'b000000:controls <= 9'b110000010;//R-TYRE
-			6'b100011:controls <= 9'b101001000;//LW
-			6'b101011:controls <= 9'b001010000;//SW
-			6'b000100:controls <= 9'b000100001;//BEQ
-			6'b001000:controls <= 9'b101000000;//ADDI
-			6'b000010:controls <= 9'b000000100;//J
+			`R_TYPE:controls <= {12'b1100000_00000,`R_TYPE_OP};//R-TYRE
+			`LW:controls <= 12'b1010010_00000_;//LW
+			`SW:controls <= 12'b0010100_00000;//SW
+			`BEQ:controls <= 12'b0001000_00000;//BEQ
+			`ADDI:controls <= 12'b1010000_00000;//ADDI
+			`J:controls <= 12'b0000001_00000;//J
 			default:  controls <= 9'b000000000;//illegal op
 		endcase
 	end
